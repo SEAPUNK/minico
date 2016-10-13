8665e4e979c192ae9ee2cdb02d2eae5f613d1316
Ivan <ivan@sq10.net>
2016-10-13 15:51:47 -0500

    code

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# minico
-It's like co, but incredibly tiny.
+`minico`
+---
+
+[![npm version](https://img.shields.io/npm/v/minico.svg?style=flat-square)](https://npmjs.com/package/minico)
+[![javascript standard style](https://img.shields.io/badge/code%20style-standard-blue.svg?style=flat-square)](http://standardjs.com/)
+[![travis build](https://img.shields.io/travis/SEAPUNK/minico/master.svg?style=flat-square)](https://travis-ci.org/SEAPUNK/minico)
+[![coveralls coverage](https://img.shields.io/coveralls/SEAPUNK/minico.svg?style=flat-square)](https://coveralls.io/github/SEAPUNK/minico)
+[![david dependencies](https://david-dm.org/SEAPUNK/minico.svg?style=flat-square)](https://david-dm.org/SEAPUNK/minico)
+[![david dev dependencies](https://david-dm.org/SEAPUNK/minico/dev-status.svg?style=flat-square)](https://david-dm.org/SEAPUNK/minico)
+
+It's like `co`, but tiny. Based off of [`copromise`](https://github.com/deanlandolt/copromise)
+
+`npm install minico`
+
+Relies on Promise being global.
+
+* 592 bytes uncompressed
+* 453 bytes minified with uglify-js 2.6.2
+* 257 bytes minified with uglify-js 2.6.2, and compressed with gzip 1.8
+
+`npm install minico`
+
+```js
+import minico from 'minico'
+
+const doThings = minico(function * doThings (input) {
+  yield washDishes()
+  try {
+      yield makePopcorn()
+  } catch (err) {
+      yield someFailureHandler(err)
+  }
+  return 'nice' + input
+})
+
+doThings(2).then((value) => {
+  console.log('Copromise success:', value) // Copromise success: nice2
+}).catch((err) => {
+  console.log('An error occured', err)
+})
+
+```
---
+++ test.js
@@ -0,0 +1,120 @@
+import test from 'ava'
+
+import delay from 'delay'
+
+import minico from './'
+
+const genfn = function * (a, b) {
+  let isCaught = false
+
+  if (a === 'hardfail1') {
+    yield Promise.reject(new Error('hardfail - 1'))
+  }
+
+  if (a === 'hardfail2') {
+    return Promise.reject(new Error('hardfail - 2'))
+  }
+
+  if (a === 'hardfail3') {
+    throw new Error('hardfail - 3')
+  }
+
+  const retV = yield 'a'
+  if (retV !== 'a') {
+    throw new Error('retV is not a!')
+  }
+
+  const retP = yield Promise.resolve(100)
+  if (retP !== 100) {
+    throw new Error('retP is not 100!')
+  }
+
+  if (b === 'stop') {
+    yield delay(100)
+    return yield Promise.resolve('ok')
+  }
+
+  try {
+    if (a === 'fail') {
+      yield Promise.reject(new Error('a is fail'))
+    } else {
+      yield Promise.resolve()
+    }
+  } catch (err) {
+    if (String(err) !== 'Error: a is fail') throw err
+    isCaught = true
+  }
+
+  return [a, b, isCaught]
+}
+
+test('normal, non-caught', async (t) => {
+  t.plan(1)
+  const coroutine = minico(genfn)
+  const retval = await coroutine(1, 2)
+  t.deepEqual(retval, [1, 2, false])
+})
+
+test('normal, caught', async (t) => {
+  t.plan(1)
+  const coroutine = minico(genfn)
+  const retval = await coroutine('fail', 2)
+  t.deepEqual(retval, ['fail', 2, true])
+})
+
+test('stopped early, with 100ms delay', async (t) => {
+  t.plan(2)
+  const coroutine = minico(genfn)
+  const startTime = Date.now()
+  const retval = await coroutine(1, 'stop')
+  t.is(retval, 'ok')
+  t.is(true, (
+    Date.now() - startTime > 100
+  ))
+})
+
+test('rejection - yield rejected promise', async (t) => {
+  t.plan(1)
+
+  const coroutine = minico(genfn)
+
+  try {
+    await coroutine('hardfail1', 2)
+  } catch (err) {
+    t.is(String(err), 'Error: hardfail - 1')
+    return
+  }
+
+  throw new Error('coroutine did not throw')
+})
+
+test('rejection - return rejected promise', async (t) => {
+  t.plan(1)
+
+  const coroutine = minico(genfn)
+
+  try {
+    await coroutine('hardfail2', 2)
+  } catch (err) {
+    t.is(String(err), 'Error: hardfail - 2')
+    return
+  }
+
+  throw new Error('coroutine did not throw')
+})
+
+test('rejection - throw error', async (t) => {
+  t.plan(1)
+
+  const coroutine = minico(genfn)
+
+  try {
+    await coroutine('hardfail3', 2)
+  } catch (err) {
+    t.is(String(err), 'Error: hardfail - 3')
+    return
+  }
+
+  throw new Error('coroutine did not throw')
+})
+
